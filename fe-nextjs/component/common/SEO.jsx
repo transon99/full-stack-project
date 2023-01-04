d27760d78f5f12745c07548031e106b7d1adbc06
transon99 <transon220599@gmail.com>
2023-01-04 18:06:09 +0700

    create layout and seo box

---
+++ fe-nextjs/component/common/SEO.jsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import React from 'react';
+
+export function SEO({ data }) {
+  const { title, description, url, thumbnailURL } = data;
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={thumbnailURL} />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={thumbnailURL}></meta>
+    </Head>
+  );
+}
--- fe-nextjs/pages/index.js
+++ fe-nextjs/pages/index.js
@@ -1,10 +1,14 @@
-
+import { SEO } from "@/component/common";
 
 export default function Home() {
   return (
 
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <SEO data={{
+      title: 'TTS Shop',
+      description: 'Shop bán máy tính siêu rẻ phù hợp với anh em chờ thủ',
+      url: 'https://full-stack-project-s7xg.vercel.app/',
+      thumbnailURL: 'https://m.media-amazon.com/images/I/713dOnHdPHL.jpg'
+    }} />
+
   )
 }
